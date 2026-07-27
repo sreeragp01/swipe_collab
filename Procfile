449d36339484914934ebb10c72bc5b6184d7729a
SREERAG <mrtuf2204@gmail.com>
2026-07-27 11:06:30 +0530

    Fix ASGI AppRegistry startup crash and port binding for Render

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne -b 0.0.0.0 -p $PORT config.asgi:application
+web: daphne -b 0.0.0.0 -p ${PORT:-10000} config.asgi:application
--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,17 @@
 import os
-import django
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
-from django.core.asgi import get_asgi_application
+# Initialize Django ASGI application early to populate AppRegistry
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
 from chat.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': JWTAuthMiddleware(
         URLRouter(websocket_urlpatterns)
     ),
